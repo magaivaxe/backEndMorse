34314cc51671258820295903fdf0f2d6bf425b1b
magaivaxe <mpg.salgad@gmail.com>
2018-04-16 14:13:12 -0400

    working on unitary Tests

---
+++ src/morse/MorseStub.java
@@ -0,0 +1,68 @@
+
+package morse;
+
+/**
+ *
+ * @author eleves
+ */
+public class MorseStub implements TraducteurMorse
+{
+
+    public MorseStub()
+    {
+    }
+
+
+    @Override
+    public String toAlpha(String morse)
+    {
+        if (morse.equals("."))
+        {
+            return "e";
+        }
+        if (morse.equals(".."))
+        {
+            return "i";
+        }
+        if (morse.equals("..."))
+        {
+            return "s";
+        }
+        return null;
+    }
+
+    @Override
+    public String toMorse(String alpha)
+    {
+        if (alpha.equals("e"))
+        {
+            return ".";
+        }
+        if (alpha.equals("i"))
+        {
+            return "..";
+        }
+        if (alpha.equals("s"))
+        {
+            return "...";
+        }
+        return null;
+    }
+
+    @Override
+    public String nettoyerAlpha(String alpha)
+    {
+        if (!alpha.isEmpty())
+        {
+            return "";
+        }
+        return null;
+    }
+
+    @Override
+    public String getNomProgrammeurs()
+    {
+        return "Stub";
+    }
+    
+}
